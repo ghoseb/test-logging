3789213ea75e15fd0706c4296fa8e1e4b0e9b389
Baishampayan Ghose <b.ghose@infinitelybeta.com>
2010-01-12 14:16:05 +0530

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject test-logging "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [log4j/log4j "1.2.14"]]
+  :dev-dependencies [[swank-clojure "1.1.0"]])
